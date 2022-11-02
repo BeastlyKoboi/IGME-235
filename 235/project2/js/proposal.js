07ad7175e42f65f4574daf5692a428ecd89eee5b
Allan Flores <itsallanflores06@gmail.com>
2022-11-02 18:01:58 -0400

    changed project 2 proposal and documentation

--- 235/project2/js/proposal.js
+++ 235/project2/js/proposal.js
@@ -1,4 +1,8 @@
+
+
 window.onload = 
     fetch('https://www.cheapshark.com/api/1.0/stores')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {console.log(data); document.querySelector("#results").innerHTML = data;} );
+
+
